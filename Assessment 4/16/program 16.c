09a866608a0c7da33cd7cada590308fd7eb5f1d3
thangameenaloshninei24-ship-it <thangameenaloshnin.ei24@bitsathy.ac.in>
2026-01-06 15:08:55 +0530

    Create program 16.c

---
+++ Assessment 4/16/program 16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int n;
+scanf("%d",&n);
+if (n <= 1)
+    printf("Not Prime");
+else {
+    int prime = 1;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            prime = 0;
+            break;
+        }
+    }
+    if (prime)
+        printf("Prime");
+    else
+        printf("Not Prime");
+}
