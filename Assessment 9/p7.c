5aa4786e944d58bce39b81767c8cc12c90f70dc7
thangameenaloshninei24-ship-it <thangameenaloshnin.ei24@bitsathy.ac.in>
2026-01-21 11:55:44 +0530

    Create p7.c

---
+++ Assessment 9/p7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100],temp;
+    for(int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+     for(int i=0;i<5;i++)
+     {
+        for(int j=i+1;j<5;j++)
+        {   
+            if(arr[i] < arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        }
+     }
+    for(int i = 0; i<5; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
